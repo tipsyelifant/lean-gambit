9ca159c545d4b23fa4a62b1cede8ec3b8aa835ac
tipsyelifant <deirepromissio@gmail.com>
2025-05-06 21:03:06 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,18 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Letter from './pages/Letter';
 import Puzzle from './pages/Puzzle';
+import Congratulations from './pages/Congratulations';
 import './styles/components.css';
 
 function App() {
   return (
     <Router>
-      <div className="layout">
+      <div className="app-container">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/letter" element={<Letter />} />
           <Route path="/puzzle/:puzzleId" element={<Puzzle />} />
+          <Route path="/congratulations" element={<Congratulations />} />
         </Routes>
       </div>
     </Router>
