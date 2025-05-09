872446ddfdfb2a67f4185d480c5516fae7166f22
tipsyelifant <deirepromissio@gmail.com>
2025-05-09 20:25:38 +0800

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import App from './App.tsx'
+import './styles/global.css'
 import './styles/components.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
