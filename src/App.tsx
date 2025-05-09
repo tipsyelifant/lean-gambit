34426b991778d7c65637cf5c8cf0d77a7af05cc2
tipsyelifant <deirepromissio@gmail.com>
2025-05-09 20:43:31 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,23 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Letter from './pages/Letter';
-import Puzzle from './pages/Puzzle';
-import Congratulations from './pages/Congratulations';
-import './styles/components.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Letter from './pages/Letter'
+import Puzzle from './pages/Puzzle'
+import Congratulations from './pages/Congratulations'
+import Registration from './pages/Registration'
+import './styles/components.css'
 
 function App() {
   return (
     <Router>
-      <div className="app-container">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/letter" element={<Letter />} />
-          <Route path="/puzzle/:puzzleId" element={<Puzzle />} />
-          <Route path="/congratulations" element={<Congratulations />} />
-        </Routes>
-      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/registration" element={<Registration />} />
+        <Route path="/letter" element={<Letter />} />
+        <Route path="/puzzle/:id" element={<Puzzle />} />
+        <Route path="/congratulations" element={<Congratulations />} />
+      </Routes>
     </Router>
-  );
+  )
 }
 
-export default App;
+export default App
