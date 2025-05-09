816f69086d42ac6b8ba175cb6627da7fb9cf6600
tipsyelifant <deirepromissio@gmail.com>
2025-05-09 21:03:16 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,21 @@ import Home from './pages/Home'
 import Letter from './pages/Letter'
 import Puzzle from './pages/Puzzle'
 import Congratulations from './pages/Congratulations'
+import Registration from './pages/Registration'
 import './styles/components.css'
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/registration" element={<Registration />} />
-        <Route path="/letter" element={<Letter />} />
-        <Route path="/puzzle/:id" element={<Puzzle />} />
-        <Route path="/congratulations" element={<Congratulations />} />
-      </Routes>
+      <div className="app-container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/registration" element={<Registration />} />
+          <Route path="/letter" element={<Letter />} />
+          <Route path="/puzzle/:id" element={<Puzzle />} />
+          <Route path="/congratulations" element={<Congratulations />} />
+        </Routes>
+      </div>
     </Router>
   )
 }
